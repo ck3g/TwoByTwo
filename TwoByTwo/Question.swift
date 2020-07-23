f26724724635e7b7d9c9ec39f6a44bf0ee06022d
Vitali Tatarintev <vtatarintev@gitlab.com>
2020-07-23 10:37:49 +0200

    Add division type to question

--- TwoByTwo/Question.swift
+++ TwoByTwo/Question.swift
@@ -19,6 +19,8 @@ struct Question {
       return "+"
     case .subtraction:
       return "-"
+    case .division:
+      return "÷"
     default:
       return "·"
     }
@@ -30,6 +32,8 @@ struct Question {
       return firstNumber + secondNumber
     case .subtraction:
       return firstNumber - secondNumber
+    case .division:
+      return firstNumber / secondNumber
     default:
       return firstNumber * secondNumber
     }
@@ -48,21 +52,30 @@ struct Question {
         let firstNumber = Int.random(in: 0..<sum)
         let secondNumber = sum - firstNumber
 
-        questions.append(Question(firstNumber: firstNumber, secondNumber: secondNumber, exerciseType: exerciseType))
+        questions.append(Question(firstNumber: firstNumber, secondNumber: secondNumber, exerciseType: .addition))
       }
 
     case .subtraction:
       for firstNumber in 1...settings.subtractionNumber {
         let secondNumber = Int.random(in: 0...firstNumber)
 
-        questions.append(Question(firstNumber: firstNumber, secondNumber: secondNumber, exerciseType: exerciseType))
+        questions.append(Question(firstNumber: firstNumber, secondNumber: secondNumber, exerciseType: .subtraction))
+      }
+
+    case .division:
+      for secondNumber in 2...settings.divisionDivider {
+        for result in 1...settings.selectedNumberOfQuestions {
+          let firstNumber = result * secondNumber
+
+          questions.append(Question(firstNumber: firstNumber, secondNumber: secondNumber, exerciseType: .division))
+        }
       }
 
     default:
       let questionsPerTable = 11
       for multiplier in 1...settings.practiceRange {
         for multiplicant in 0..<questionsPerTable {
-          questions.append(Question(firstNumber: multiplier, secondNumber: multiplicant, exerciseType: exerciseType))
+          questions.append(Question(firstNumber: multiplier, secondNumber: multiplicant, exerciseType: .multiplication))
         }
       }
     }
@@ -71,10 +84,10 @@ struct Question {
   }
 
   func generateAnswerSuggestions() -> [(value: Int, isCorrect: Bool)] {
-    var diff = self.firstNumber == 0 ? 1 : self.firstNumber
+    var diff = Int.random(in: 1...2)
 
-    if self.exerciseType == .addition || self.exerciseType == .subtraction {
-      diff = Int.random(in: 1...2)
+    if self.exerciseType == .multiplication {
+      diff = self.firstNumber == 0 ? 1 : self.firstNumber
     }
 
     var answers = [
